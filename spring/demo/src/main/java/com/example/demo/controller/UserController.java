37f62aa37fea82170e05d26d7842eabbd4b13330
tran.thanh.son <tran.thanh.son@sun-asterisk.com>
2025-10-13 22:48:29 +0700

    spring demo

---
+++ spring/demo/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
